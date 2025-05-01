ab807967edc436a7a2aaf33b780eb6b9669e9115
John Kearon <jkearon@hotmail.com>
2025-05-01 09:33:43 -0400

    Initial Commit

---
+++ ESSI/ESSIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ESSIApp.swift
+//  ESSI
+//
+//  Created by John Kearon on 5/1/25.
+//
+
+import SwiftUI
+
+@main
+struct ESSIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
